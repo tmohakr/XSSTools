5bf489b780427f195842e029cee04b57c204580f
tmohakr <119854114+tmohakr@users.noreply.github.com>
2024-01-31 16:10:07 +0000

    Add files via upload

---
+++ perl_hello.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print "Content-Type: text/html\n\n";
+print "<html><head><title>Perl Script Result</title></head><body>";
+print "<h1>Hello from Perl!</h1>";
+print "</body></html>";
